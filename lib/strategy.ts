d18c37d607cc2296e3227de387825d5145a33136
BFKKuTu Official <lshqqytiger@naver.com>
2021-02-26 01:12:14 +0900

    url

--- lib/strategy.ts
+++ lib/strategy.ts
@@ -8,13 +8,12 @@ import uid from "uid2";
 import { Profile } from "./types";
 import parseProfile from "./profile";
 
-const defaultAuthorizationURL = "https://daldal.so/oauth/authorize";
-const defaultTokenURL = "https://daldal.so/oauth/token";
-const defaultUserProfileURL = "https://daldal.so/oauth/api/me";
+const providerURL = "https://daldal.so";
 
 const setupOptions = (options: StrategyOptions) => {
-  options.authorizationURL = defaultAuthorizationURL;
-  options.tokenURL = defaultTokenURL;
+  options.authorizationURL =
+    options.authorizationURL || `${providerURL}/oauth/authorize`;
+  options.tokenURL = options.tokenURL || `${providerURL}/oauth/token`;
   options.state = uid(24);
 
   return options;
@@ -33,33 +32,37 @@ export default class Strategy extends OAuth2Strategy {
     accessToken: string,
     done: (error: Error | null, profile?: any) => void
   ) {
-    this._oauth2.get(defaultUserProfileURL, accessToken, (err, body, res) => {
-      if (err) {
-        return done(
-          new InternalOAuthError("Failed to fetch user profile", err)
-        );
+    this._oauth2.get(
+      `${providerURL}/oauth/api/me`,
+      accessToken,
+      (err, body, res) => {
+        if (err) {
+          return done(
+            new InternalOAuthError("Failed to fetch user profile", err)
+          );
+        }
+
+        const _body = body ? body.toString() : "";
+
+        try {
+          body = JSON.parse(_body);
+        } catch (err) {
+          return done(
+            new InternalOAuthError("Failed to parse API response", err)
+          );
+        }
+
+        if (!body) {
+          return done(new Error("Empty API Response"));
+        }
+
+        const profile: Profile = parseProfile(body);
+        profile._raw = _body;
+        profile._json = JSON.parse(_body);
+
+        done(null, profile);
       }
-
-      const _body = body ? body.toString() : "";
-
-      try {
-        body = JSON.parse(_body);
-      } catch (err) {
-        return done(
-          new InternalOAuthError("Failed to parse API response", err)
-        );
-      }
-
-      if (!body) {
-        return done(new Error("Empty API Response"));
-      }
-
-      const profile: Profile = parseProfile(body);
-      profile._raw = _body;
-      profile._json = JSON.parse(_body);
-
-      done(null, profile);
-    });
+    );
   }
 }
 
